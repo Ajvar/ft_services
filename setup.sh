cba0e0de11471d5f2b5be440a48d43a4ee702da1
Joscelyn CUEILLE <jcueille@e2r6p4.42.fr>
2020-06-18 18:52:16 +0200

    dashboard

--- setup.sh
+++ setup.sh
@@ -16,11 +16,14 @@ then
         fi
 minikube addons enable ingress
 fi
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
-kubectl apply -f srcs/dashboard-admin-user.yaml
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') | grep "token:" |cut -d " " -f 7 > srcs/token
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+#kubectl apply -f srcs/dashboard-admin-user.yaml
+#kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') | grep "token:" |cut -d " " -f 7 > srcs/token
 eval $(minikube docker-env)
 docker build -t 42nginx:v1 srcs/nginx/
+docker build -t 42ftps:v1 srcs/ftps/
 kubectl apply -f nginx.yaml
 kubectl apply -f ingress.yaml
+kubectl apply -f ftps.yaml
 
+minikube dashboard
--- srcs/FTPS/entrypoint.sh
+++ srcs/FTPS/entrypoint.sh
@@ -1,9 +1,11 @@
-[ -z $FTPS_USER ] && FTPS_USER=admin
-[ -z $FTPS_PASS ] && FTPS_PASS=password
+#!/bin/sh
 
-adduser -D admin
+[ -z "$FTPS_USER" ] && FTPS_USER=admin
+[ -z "$FTPS_PASS" ] && FTPS_PASS=password
 
-echo "$FTPS_USER:$FTPS_PASS" | chpasswd
-Minikube_ip=`minikube ip`
+adduser -D "$FTPS_USER"
 
-/usr/local/sbin/pure-ftpd -Y 2 -P $Minikube_ip
+#echo "$FTPS_USER:$FTPS_PASS" | chpasswd
+#Minikube_ip=`minikube ip`
+
+/usr/local/sbin/pure-ftpd -Y 2 #-P $Minikube_ip
