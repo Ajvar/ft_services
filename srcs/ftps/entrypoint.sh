464347c02c0ae8d95999a120252d584a62e5cca9
Ajvar <cueille@protonmail.com>
2020-06-19 20:00:23 +0200

    ftps

---
+++ srcs/ftps/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+[ -z "$FTPS_USER" ] && FTPS_USER=admin
+[ -z "$FTPS_PASS" ] && FTPS_PASS=password
+
+adduser -D "$FTPS_USER"
+
+echo "$FTPS_USER:$FTPS_PASS" | chpasswd
+#Minikube_ip=`minikube ip`
+
+/usr/sbin/pure-ftpd -Y 2 #-P $Minikube_ip
+/bin/sh
