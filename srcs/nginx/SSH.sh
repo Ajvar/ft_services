b06dafef18d3a223b3590603e7cb61b3fe907519
Ajvar <cueille@protonmail.com>
2020-09-24 18:53:50 +0200

    coucou mli

--- srcs/nginx/SSH.sh
+++ srcs/nginx/SSH.sh
@@ -7,5 +7,6 @@ adduser -D "$SSH_USER"
 echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 echo "user:password = $SSH_USER:$SSH_PASSWORD"
 
-/usr/sbin/sshd
-/usr/sbin/nginx -g 'daemon off;'
+ssh-keygen -A
+
+supervisord
