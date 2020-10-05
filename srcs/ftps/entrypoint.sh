0144658c71acd8c0bf2cd81dbc81cdb138500613
ajvar <cueille@protonmail.com>
2020-10-05 10:54:59 +0200

    ftps working in container. K8 service returning forbidenn address error.

--- srcs/ftps/entrypoint.sh
+++ srcs/ftps/entrypoint.sh
@@ -9,5 +9,5 @@ echo "$FTPS_USER:$FTPS_PASS" | chpasswd
 #Minikube_ip=`minikube ip`
 
 #/usr/sbin/pure-ftpd -Y 2 #-P $Minikube_ip
-#supervisord -c /etc/supervisord.conf
-sh
+supervisord -c /etc/supervisord.conf
+#sh
