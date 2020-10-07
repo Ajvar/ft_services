72a4de2333771bf59327ade70f13a8c8e82cc906
ajvar <cueille@protonmail.com>
2020-10-07 23:08:47 +0200

    can connect to ftps but can't see the file, must set the defqult ftp directory

--- srcs/ftps/entrypoint.sh
+++ srcs/ftps/entrypoint.sh
@@ -3,7 +3,9 @@
 [ -z "$FTPS_USER" ] && FTPS_USER=admin
 [ -z "$FTPS_PASS" ] && FTPS_PASS=password
 
-adduser -D "$FTPS_USER"
-
+adduser -D "$FTPS_USER" -h /mnt/vsftpd
+mkdir -p /mnt/vsftpd/Test
+touch /mnt/vsftpd/Test/test.txt
+echo "Bonne correction !" >> /mnt/vsftpd/Test/test.txt
 echo "$FTPS_USER:$FTPS_PASS" | chpasswd
 supervisord -c /etc/supervisord.conf
