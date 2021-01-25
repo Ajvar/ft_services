d8ade27dc18d2c362b6817e94b046679f82d8d4f
ajvar <cueille@protonmail.com>
2021-01-25 22:49:38 +0100

    All done, must test with blank VM

--- dock.sh
+++ dock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-var=$(docker ps | grep "k8s_$1_$1-0" | cut -d ' ' -f1)
+var=$(docker ps | grep "$1" | cut -d ' ' -f1)
 echo $var
 docker exec -it $var sh; docker kill $var 2>/dev/null 1>&2
--- ft_reset.sh
+++ ft_reset.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+pod=$(sudo kubectl get po | grep $1 | cut -d " " -f 1)
 sudo kubectl delete statefulset $1
+sudo kubectl delete deploy $1
 sudo kubectl delete svc $1
 sudo kubectl delete pvc $1-pvc
-sudo kubectl delete pod $1-0
+sudo kubectl delete pod $pod
 sudo kubectl apply -f srcs/yamls/modified-yamls/$1.yaml
 
